7dbc84e68b138c02d5a1a977f19783bdae331116
Grigory Dryapak <grigory.dryapak@gmail.com>
2017-05-17 22:06:45 +0300

    filestorage: upload to s3 using s3manager

--- filestorage/s3.go
+++ filestorage/s3.go
@@ -1,15 +1,14 @@
 package filestorage
 
 import (
-	"bytes"
 	"fmt"
 	"io"
-	"io/ioutil"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 // AmazonS3 is an Amazon S3 file storage.
@@ -32,48 +31,36 @@ func NewAmazonS3(accessKey, secretKey, region, bucket string) (*AmazonS3, error)
 		bucket: bucket,
 		svc:    s3.New(sess),
 	}
-
 	return s, nil
 }
 
 // Save saves data from r to file with the given path.
 func (s *AmazonS3) Save(path string, r io.Reader) error {
-	var body io.ReadSeeker
-	if rs, ok := r.(io.ReadSeeker); ok {
-		body = rs
-	} else {
-		buf, err := ioutil.ReadAll(r)
-		if err != nil {
-			return fmt.Errorf("failed to read from r: %s", err)
-		}
-		body = bytes.NewReader(buf)
+	_, err := s3manager.NewUploaderWithClient(s.svc).Upload(
+		&s3manager.UploadInput{
+			Bucket: aws.String(s.bucket),
+			Key:    aws.String(path),
+			ACL:    aws.String(s3.ObjectCannedACLPublicRead),
+			Body:   r,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to upload object to S3: %s", err)
 	}
-
-	params := &s3.PutObjectInput{
-		Bucket: aws.String(s.bucket),
-		Key:    aws.String(path),
-		Body:   body,
-		ACL:    aws.String(s3.ObjectCannedACLPublicRead),
-	}
-
-	if _, err := s.svc.PutObject(params); err != nil {
-		return fmt.Errorf("failed to put object to s3: %s", err)
-	}
-
 	return nil
 }
 
 // Remove removes the file with the given path.
 func (s *AmazonS3) Remove(path string) error {
-	params := &s3.DeleteObjectInput{
-		Bucket: aws.String(s.bucket),
-		Key:    aws.String(path),
+	_, err := s.svc.DeleteObject(
+		&s3.DeleteObjectInput{
+			Bucket: aws.String(s.bucket),
+			Key:    aws.String(path),
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to delete object from S3: %s", err)
 	}
-
-	if _, err := s.svc.DeleteObject(params); err != nil {
-		return fmt.Errorf("failed to delete object from s3: %s", err)
-	}
-
 	return nil
 }
 
